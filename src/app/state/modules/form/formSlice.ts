f9fa101c22152d77e6de8c812a9e09faa40449c9
Alisha <alisha.shrestha@logicabeans.com>
2022-07-26 16:45:10 +0545

    sidenav and formDetails page

---
+++ src/app/state/modules/form/formSlice.ts
@@ -0,0 +1,21 @@
+import { initialFormState } from "./form.initial";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { FormState } from "./form.types";
+
+export const formSlice = createSlice({
+  name: "form",
+  initialState: initialFormState,
+  reducers: {
+    SET_BUS_DATA: (state, action: PayloadAction<FormState>) => {
+      state.name = action.payload.name;
+      state.password = action.payload.password;
+      state.email = action.payload.email;
+      state.nationality = action.payload.nationality;
+      state.gender = action.payload.gender;
+      state.hobbies = action.payload.hobbies;
+    },
+  },
+});
+
+export const { SET_BUS_DATA } = formSlice.actions;
+export default formSlice.reducer;
